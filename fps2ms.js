02b7de8002b14171e81dfc33f0cf3c8add301e57
Unzor <71789103+Unzor@users.noreply.github.com>
2022-02-13 13:21:43 -0600

    Update and rename fps2ms.js to fps2time.js

--- fps2ms.js
+++
@@ -1,3 +0,0 @@
-function fps_to_ms(fps) {
-return fps * 0.277777778;
-}
---
+++ fps2time.js
@@ -0,0 +1,7 @@
+function fps_to_ms(fps) {
+return fps * 0.277777778;
+}
+
+function fps_to_seconds(fps) {
+return (fps * 0.277777778) / 1000;
+}
